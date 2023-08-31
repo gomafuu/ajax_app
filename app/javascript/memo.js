831bc685c8f89eaa8709bebba420763e840071f5
gomafuu <a.akane.0207.trb@gmail.com>
2023-08-31 18:08:49 +0900

    JSでレスポンスを処理する

--- app/javascript/memo.js
+++ app/javascript/memo.js
@@ -1,3 +1,16 @@
+const buildHTML = (XHR) => {
+  const item = XHR.response.post;
+  const html = `
+    <div class="post">
+      <div class="post-date">
+        投稿日時：${item.created_at}
+      </div>
+      <div class="post-content">
+        ${item.content}
+      </div>
+    </div>`;
+  return html;
+};
 function post (){
   const submit = document.getElementById("submit");
   submit.addEventListener("click", (e) => {
@@ -13,6 +26,19 @@ function post (){
     XHR.responseType = "json";
     // リクエストを送信
     XHR.send(formData);
+    // リクエスト送信成功で呼び出されるプロパティ
+    XHR.onload = ()=> {
+      if(XHR.status != 200){
+        alert(`Error ${XHR.status}:${XHR.statusText}`);
+        // エラー時以降の処理を行わないようにするため。JSの処理から抜けだす。
+        return nulll;
+      };
+      const list = document.getElementById("list");
+      const formText = document.getElementById("content");
+        // list要素を要素内部最後の子要素の直後に挿入
+        list.insertAdjacentHTML("afterend",html);
+        formText.value = "";
+    };
   });
 };
 
