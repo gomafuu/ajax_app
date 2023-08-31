ae22db87b781c54343d5655233a6f5f56f3ae7fc
gomafuu <a.akane.0207.trb@gmail.com>
2023-08-31 17:10:45 +0900

    リクエストを処理する

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,12 @@
 class PostsController < ApplicationController
-
   def index
     @posts = Post.order(id: "DESC")
   end
-
-  # def new
-  # end
-
+ 
   def create
-    Post.create(content: params[:content])
+    # 新たに投稿されたメモの内容を変数postに格納↓
+    post = Post.create(content: params[:content])
+    # 返却されるデータフォーマットをjsonmに指定
+    render json:{post: post}
   end
-end
+ end
